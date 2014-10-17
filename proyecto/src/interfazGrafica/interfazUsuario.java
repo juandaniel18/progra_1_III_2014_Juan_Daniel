8d9f2e4f9777a2549c708ab245d79496da4765bc
juandaniel18 <ginette_cordero@hotmail.com>
2014-10-16 21:41:34 -0600

    Proyecto

--- proyecto/src/interfazGrafica/interfazUsuario.java
+++ proyecto/src/interfazGrafica/interfazUsuario.java
@@ -3,36 +3,39 @@ package interfazGrafica;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
+import javax.swing.JPanel;
 
 public class interfazUsuario {
 
     JFrame tablero = new JFrame("Cuatro en línea");
     JButton botones[][] = new JButton[6][7];
     FlowLayout layout = new FlowLayout();
+    JPanel panel = new JPanel();
 
     public void configurarTablero() {
         tablero.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         tablero.setSize(500, 300);
+        tablero.add(panel);
         tablero.setVisible(true);
     }
 
     public void asignarLayout() {
-        tablero.setLayout(layout);
+        panel.setLayout(layout);
     }
 
-//    public void agregarBotones() {
-//        for (int i = 0; i < 6; i++) {
-//            for (int j = 0; j < 7; j++) {
-//                tablero.add(botones[i][j]);
-//            }
-//        }
-//    }
+    public void agregarBotones() {
+        for (int i = 0; i < botones.length; i++) {
+            for (int j = 0; j < botones[0].length; j++) {
+                panel.add(botones[i][j]=new JButton());
+            }
+        }
+    }
 
     public static void main(String[] args) {
         interfazUsuario t = new interfazUsuario();
         t.configurarTablero();
         t.asignarLayout();
-//        t.agregarBotones();
+        t.agregarBotones();
 
     }
 
